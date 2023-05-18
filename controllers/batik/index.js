6c2b9f7aeeb7d1e3da1fd664d85bea8b5159d138
daffaRadhitya <111202012648@mhs.dinus.ac.id>
2023-05-18 16:47:27 +0700

    create daffa-dev

--- controllers/batik/index.js
+++ controllers/batik/index.js
@@ -1,8 +1,8 @@
 module.exports = {
     getAllBatik : (req,res)=>{
         res.json({
-            message:"berhasil get batik",
-            status:res.statusCode
+            message:"berhasil get batik still in one method",
+            status:res.statusCode 
         })
 
     }
