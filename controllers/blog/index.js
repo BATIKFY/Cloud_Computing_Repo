6ddaf90a742b860e82daa655d3452614f9a8ea16
daffaRadhitya <111202012648@mhs.dinus.ac.id>
2023-05-23 07:44:41 +0700

    complete crud blog

--- controllers/blog/index.js
+++ controllers/blog/index.js
@@ -84,5 +84,70 @@ module.exports = {
             data: null
             });
        }
+    },
+    updateBlog : async (req,res) => {
+        try {
+           const {id} = req.params
+           const updatedBlog = await blogModel.update(req.body,{
+            where:{
+                id:id,
+            }
+           }) 
+           if (!updatedBlog[0]) {
+            return res.status(200).json({
+                status: 200,
+                success: false,
+                message: "failed to update blog, cant find the id",
+                data: null
+              });
+           }else{
+            return res.status(200).json({
+                status: 200,
+                success: true,
+                message: "blog updated",
+                data: {
+                    blog: req.body,
+                  }
+              });
+           }
+        } catch (error) {
+            return res.status(500).json({
+                status: 500,
+                success: false,
+                message: "internal server error",
+                data: null
+                });
+        }
+    },
+    deleteBlog : async (req, res)=>{
+        try {
+            const {id} =  req.params
+            const deleteBlog = await blogModel.destroy({
+                where:{
+                    id:id
+                }
+            })
+            if (!deleteBlog) {
+                return res.status(200).json({
+                    status: 200,
+                    success: false,
+                    message: "failed to delete blog, cant find the batik",
+                    data: null
+                  });                
+            }else{
+                return res.status(200).json({
+                    status: 200,
+                    success: false,
+                    message: "blog deleted successfully"
+                  });
+            }
+        } catch (error) {
+            return res.status(500).json({
+                status: 500,
+                success: false,
+                message: "internal server error",
+                data: null
+              });
+        }
     }
 }
--- routes/blog/index.js
+++ routes/blog/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const {getAllblog,postBlog,getBlogbyId} = require("../../controllers/blog")
+const {getAllblog,postBlog,getBlogbyId,updateBlog,deleteBlog} = require("../../controllers/blog")
 
 
 router.get('/',getAllblog)
 router.get('/:id',getBlogbyId)
 router.post('/',postBlog)
+router.put('/:id',updateBlog)
+router.delete('/:id',deleteBlog)
 
 
 
