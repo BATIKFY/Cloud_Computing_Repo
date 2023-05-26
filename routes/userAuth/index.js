c72463c4cfdfb81be31398d0317a98bd24284f38
daffaRadhitya <111202012648@mhs.dinus.ac.id>
2023-05-26 10:50:08 +0700

    add login

--- routes/userAuth/index.js
+++ routes/userAuth/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 const router = express.Router()
 const {registerUser} = require('../../controllers/user/register')
+const {getAllUser,getUserById,loginUser} = require('../../controllers/user/login')
 
-
-router.post('/login',registerUser)
+router.post('/login',loginUser)
+router.post('/register',registerUser)
+router.get('/:id',getUserById)
+router.get('/',getAllUser)
 // router.use('/signup')
 
 
